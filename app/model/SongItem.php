22dda93ca2899fe69778ef8ac93fc9e225a7cf97
DNepovim <jsem@dominikblaha.cz>
2017-08-03 22:30:52 +0200

    Get lyric in HTML function

--- app/model/SongItem.php
+++ app/model/SongItem.php
@@ -73,8 +73,7 @@ class SongItem extends Nette\Object
 			$this->title = $song->title;
 			$this->guid = $song->guid;
 			$this->interpreter = $song->interpreter;
-			$this->lyric = $this->markupParser($song->lyric);
-			$this->lyricSource= $song->lyric;
+			$this->lyric = $song->lyric;
 			$this->songbooks = $this->getUsedSongbooksIDs();
 
 			return true;
@@ -111,8 +110,7 @@ class SongItem extends Nette\Object
 			$this->title = $song->title;
 			$this->guid = $song->guid;
 			$this->interpreter = $song->interpreter;
-			$this->lyric = $this->markupParser($song->lyric);
-			$this->lyricSource= $song->lyric;
+			$this->lyric = $song->lyric;
 			$this->songbooks = $this->getUsedSongbooksIDs();
 
 			return true;
@@ -142,64 +140,104 @@ class SongItem extends Nette\Object
 	}
 
 	/**
-	 * Markup parser.
-	 * @param  string Markup text
+	 * Parse markup 
+	 * @param string Markup text
+	 * @param array Parser options
 	 * @return string HTML text
 	 */
-	protected function markupParser($text) {
-
-		$verseOpenTag = '<span class="chord-verse">';
-		$verseCloseTag = '</span>';
-		$verseTag = $verseOpenTag . $verseCloseTag;
-		$chorusTag = '<span class="chord-chorus">R:</span>';
-		$chordOpenTag = '<span class="chord"><span>';
-		$chordCloseTag = '</span></span>';
-		$chordLineOpenTag = '<div class="chord-line">';
-		$chordLineCloseTag = '</div>';
+	protected function markupParser($text, $options) 
+	{
+		
+		$options['verseOpenTag'] = isset($options['verseOpenTag']) ? $options['verseOpenTag'] : '';
+		$options['verseCloseTag'] = isset($options['verseCloseTag']) ? $options['verseCloseTag'] : '';
+		$options['verseEndTag'] = isset($options['verseEndTag']) ? $options['verseEndTag'] : '';
+		$options['verseTag'] = $options['verseOpenTag'] . $options['verseCloseTag'];
+		$options['chorusOpenTag'] = isset($options['chorusOpenTag']) ? $options['chorusOpenTag'] : '';
+		$options['chorusEndTag'] = isset($options['chorusEndTag']) ? $options['chorusEndTag'] : '';
+		$options['chordOpenTag'] = isset($options['chordOpenTag']) ? $options['chordOpenTag'] : '';
+		$options['chordCloseTag'] = isset($options['chordCloseTag']) ? $options['chordCloseTag'] : '';
+		$options['chordLineOpenTag'] = isset($options['chordLineOpenTag']) ? $options['chordLineOpenTag'] : '';
+		$options['chordLineCloseTag'] = isset($options['chordLineCloseTag']) ? $options['chordLineCloseTag'] : '';
+		$options['endOfLine'] = isset($options['endOfLine']) ? $options['endOfLine'] : '';
+		$options['hellip'] = isset($options['hellip']) ? $options['hellip'] : '';
 
 		$pattern[] = '/<[v|s]>/';
 		$pattern[] = '/<(ch|r)>/';
 		$pattern[] = '/<([a-zA-Z1-9#]+[\/]?[a-zA-Z1-9#]*)>/';
-		$replacement[] = $verseTag;
-		$replacement[] = $chorusTag;
-		$replacement[] = $chordOpenTag . '$1' . $chordCloseTag;
+		$replacement[] = $options['verseTag'];
+		$replacement[] = $options['chorusOpenTag'];
+		$replacement[] = $options['chordOpenTag'] . '$1' . $options['chordCloseTag'];
 		$markuped = nl2br(preg_replace($pattern, $replacement, $text));
 
 		$line_list = explode('<br />', $markuped);
 
 		$i = 0;
 		$verse = 0;
+		$isOpenedVerse = false;
+		$isOpenedChorus = false;
 		foreach ($line_list as $line) {
 
 			// Remove all '<br />' tags
 			$line_list[$i] = str_replace('<br />', '', $line_list[$i]);
 
 			// Check if is some chords in line
-			if (strpos($line_list[$i], $chordOpenTag)) {
+			if (strpos($line_list[$i], $options['chordOpenTag']) !== false) {
 				// Wrap line with chords
-				$line_list[$i] = $chordLineOpenTag . $line_list[$i] . $chordLineCloseTag;
-			} elseif (!empty(trim($line_list[$i]))&&!strpos($verseCloseTag, trim($line_list[$i]))&&!strpos($verseOpenTag, trim($line_list[$i]))&&!strpos($chorusTag, trim($line_list[$i]))) {
+				$line_list[$i] = $options['chordLineOpenTag'] . $line_list[$i] . $options['chordLineCloseTag'];
+			} elseif (!empty(trim($line_list[$i]))&&!(strpos($options['verseCloseTag'], trim($line_list[$i]))!==false)&&!(strpos($options['verseOpenTag'], trim($line_list[$i]))!==false)&&!(strpos($options['chorusOpenTag'], trim($line_list[$i]))!==false)) {
 				// Break line without chords
-				$line_list[$i] .= '<br>';
+				$line_list[$i] .= $options['endOfLine'];
 			}
 
 			// Process verse tag
-			if (strpos($line_list[$i], $verseTag)) {
-				$line_list[$i] = str_replace($verseTag, $verseOpenTag . ++$verse . '.' . $verseCloseTag, $line_list[$i]);
+			if (strpos($line_list[$i], $options['verseTag']) !== false) {
+				$line_list[$i] = str_replace($options['verseTag'], $options['verseOpenTag'] . ++$verse . '.' . $options['verseCloseTag'], $line_list[$i]);
+				if ($isOpenedVerse) {
+					$line_list[$i-1] .= $options['verseEndTag'];
+				}
+				if ($isOpenedChorus) {
+					$line_list[$i-1] .= $options['chorusEndTag'];
+					$isOpenedChorus = false;
+				}
+				$isOpenedVerse = true;
 			}
 
 			// Process chorus tag
-			if (strpos($line_list[$i], $chorusTag)) {
+			if (strpos($line_list[$i], $options['chorusOpenTag']) !== false) {
 				// Check if is on line only chorus tag
-				if (trim($line_list[$i]) !== $chorusTag . '<br>') {
+				if (trim($line_list[$i]) !== $options['chorusOpenTag'] . $options['endOfLine']) {
 					// Save first line of chorus
 					$chorusLine = $line_list[$i];
-					$chorusLine = preg_replace('#' . addcslashes($chordOpenTag, '<>/"') . '([a-zA-Z1-9]*)' . addcslashes($chordCloseTag, '<>/"') . '#', '', $chorusLine);
-					$chorusLine = rtrim(rtrim($chorusLine, '</div>'), ',') . '&hellip;</div>';
+					// Remove chords
+					$chorusLine = preg_replace('#' . addcslashes($options['chordOpenTag'], '<>/"') . '([a-zA-Z1-9]*)' . addcslashes($options['chordCloseTag'], '<>/"') . '#', '', $chorusLine);
+					$chorusLine = rtrim(rtrim($chorusLine, $options['chordLineCloseTag']), ',.') . $options['hellip'] . $options['chordLineCloseTag'];
+
+					if ($isOpenedVerse) {
+						$line_list[$i-1] .= $options['verseEndTag'];
+						$isOpenedVerse = false;
+					}
+
+					if ($isOpenedChorus) {
+						$line_list[$i-1] .= $options['chorusEndTag'];
+					}
+
+					$isOpenedChorus = true;
+
 				} else {
 					// Add first line of chorus
 					if (!empty($chorusLine)) {
 						$line_list[$i] = $chorusLine;
+						
+						if ($isOpenedVerse) {
+							$line_list[$i-1] .= $options['verseEndTag'];
+							$isOpenedVerse = false;
+						}
+
+						if ($isOpenedChorus) {
+							$line_list[$i-1] .= $options['chorusEndTag'];
+						}
+
+						$isOpenedChorus = true;
 					}
 				}
 			}
@@ -210,7 +248,33 @@ class SongItem extends Nette\Object
 		// Implode lines to one string
 		$lyric = implode($line_list);
 
+		if ($isOpenedChorus) {
+			$lyric .= $options['chorusEndTag'];
+		} elseif ($isOpenedVerse) {
+			$lyric .= $options['verseEndTag'];
+		}
+
 		return $lyric;
 	}
 
+	/**
+	 * Create HTML from markup
+	 * @param  string Markup text
+	 * @return string HTML text
+	 */
+	public function getLyricInHtml() 
+	{
+		$options['verseOpenTag'] = '<span class="chord-verse">';
+		$options['verseCloseTag'] = '</span>';
+		$options['chorusOpenTag'] = '<span class="chord-chorus">R:</span>';
+		$options['chordOpenTag'] = '<span class="chord"><span>';
+		$options['chordCloseTag'] = '</span></span>';
+		$options['chordLineOpenTag'] = '<div class="chord-line">';
+		$options['chordLineCloseTag'] = '</div>';
+		$options['endOfLine'] = '<br>';
+		$options['hellip'] = '&hellip;';
+
+		return $this->markupParser($this->lyric, $options);
+	}
+	
 }
